72a6e750c307ce5962ed4effad9f90b7271e4b54
Taeyoon Kim <tykimseoul@gmail.com>
2019-08-20 18:06:40 +0900

    add integrals

--- read.py
+++ read.py
@@ -1,12 +1,13 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.integrate as integrate
 
 with open('data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     time = []
-    value = []
+    acceleration = []
     for row in csv_reader:
         if line_count == 0:
             print(f'Column names are {", ".join(row)}')
@@ -14,14 +15,30 @@ with open('data.csv') as csv_file:
         else:
             print(row)
             time.append(int(row[0]))
-            value.append(float(row[1]))
+            acceleration.append(float(row[1]))
             line_count += 1
     time = np.array(time)
-    value = np.array(value)
+    acceleration = np.array(acceleration)
     time = time - min(time)
-    plt.plot(time, value)
-    plt.xlabel("time (ms)")
+    time = time / 1000
+    velocity = integrate.cumtrapz(acceleration, time)
+    distance = integrate.cumtrapz(velocity, time[:-1])
+    plt.plot(time, acceleration, '-b')
+    plt.plot(time, acceleration, 'ro', markersize=3)
+    plt.xlabel("time (s)")
     plt.ylabel("z-acceleration (m/s^2)")
-    plt.savefig("plot.png")
+    plt.savefig("accel.png")
+    plt.show()
+    plt.figure()
+    plt.plot(time[:-1], velocity, '-g')
+    plt.xlabel('time (s)')
+    plt.ylabel('velocity (m/s)')
+    plt.savefig("velocity.png")
+    plt.show()
+    plt.figure()
+    plt.plot(time[:-2], distance, '-b')
+    plt.xlabel('time (s)')
+    plt.ylabel('depth (m)')
+    plt.savefig('distance.png')
     plt.show()
     print(f'Processed {line_count} lines.')
